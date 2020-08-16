8d1b49d9a6fa1b6c0fe96aeef8474e267f0a9e03
omiend <omiend@gmail.com>
2020-08-16 14:45:59 +0900

    ReactRouterを導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,44 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Trigonometric from "./Trigonometric/index";
+import Tier from "./Tier/index";
 
 import "destyle.css";
 import "./css/main.css";
 
 class App extends React.Component {
-	public render() {
-		return <Trigonometric />;
-	}
+  public render() {
+    return (
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/trigonometric">Trigonometric</Link>
+            </li>
+            <li>
+              <Link to="/tier">Tierd Priching</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/trigonometric">
+            <Trigonometric />
+          </Route>
+          <Route path="/tier">
+            <Tier />
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 ReactDom.render(<App />, document.getElementById("root"));
